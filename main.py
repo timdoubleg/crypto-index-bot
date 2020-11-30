2ceb362c2ba0c40ce7ff082ea8b453061414a3b4
timggraf <tim.g.graf@gmail.com>
2020-11-30 18:11:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-    
-    from binance.client import Client # Import the Binance Client
-    from binance.websockets import BinanceSocketManager # Import the Binance Socket Manager
 
-    # Although fine for tutorial purposes, your API Keys should never be placed directly in the script like below. 
-    # You should use a config file (cfg or yaml) to store them and reference when needed.
-    PUBLIC = 'r7NNoC9Y67xf2mmwSTYM1DwRA03Q3i6YHvKElp9aU6a3LFh0Fhmv0MRPSqBsAt6z'
-    SECRET = 'CTfkY0bkUpiLfzEgZjL78X93UhN79Tb26Cqp7W27TSVvhod8vZUR3ACr1Q0B86ju'
+from binance.client import Client # Import the Binance Client
+from binance.websockets import BinanceSocketManager # Import the Binance Socket Manager
+import pandas as pd
 
-    # Instantiate a Client 
-    client = Client(api_key=PUBLIC, api_secret=SECRET)
+# Although fine for tutorial purposes, your API Keys should never be placed directly in the script like below. 
+# You should use a config file (cfg or yaml) to store them and reference when needed.
+PUBLIC = 'r7NNoC9Y67xf2mmwSTYM1DwRA03Q3i6YHvKElp9aU6a3LFh0Fhmv0MRPSqBsAt6z'
+SECRET = 'CTfkY0bkUpiLfzEgZjL78X93UhN79Tb26Cqp7W27TSVvhod8vZUR3ACr1Q0B86ju'
 
-    # Gets data from account balance as dictionary
-    coin_balance = client.get_account()
-    print(coin_balance)
+# Instantiate a Client 
+client = Client(api_key=PUBLIC, api_secret=SECRET)
 
-    # Initializes empty list to store our allocations
-    allocations = []
+# Gets data from account balance as dictionary
+coin_balance = client.get_account()
+print(coin_balance)
 
-    # Loops through the account balance and stores only the allocations > 0
-    for element in coin_balance['balances']:
-        if float(element.get('free')) > 0.00000000:
-            alloc = str(element).replace("{","").replace("}","").replace("'","")
-            allocations.append(alloc)
+#from dictionary to dataframe
+pd.DataFrame.from_dict(coin_balance['balances'])
 
-    print(allocations)
+#get market caps
+marketcap = cg.get_global() #get the marketcap
 

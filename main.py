74b5c82462d28fb516a54907147586d507953205
marvinscherer <marvin_scherer@hotmail.com>
2020-11-30 20:36:23 +0100

    updated main

--- main.py
+++ main.py
@@ -16,13 +16,23 @@ client = Client(api_key=PUBLIC, api_secret=SECRET)
 # Gets data from account balance as dictionary
 coin_balance = client.get_account()
 
-#from dictionary to dataframe
+# From dictionary to dataframe
 coin_balance = pd.DataFrame.from_dict(coin_balance['balances'])
+
+# Drops all the cryptocurrencies and leaves only the ones where we have money allocated
+coin_balance["free"] = pd.to_numeric(coin_balance["free"], downcast="float")
+coin_balance = coin_balance[coin_balance["free"] > 0]
 print(coin_balance)
 
-#get market caps
+# Gets market caps
 market_cap = pd.DataFrame.from_dict(cg.get_global())
 market_cap_perc = market_cap["market_cap_percentage"].sort_values(ascending = False).head(10)
-
 print(market_cap_perc)
+
+# Computes the sum of all the percentages
+total_market_cap = market_cap_perc.sum()
+print(total_market_cap)
+
+
+
 

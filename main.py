696bdfcc320f1568bcf07a2bc8b5910935761489
Henri de Montpellier <henridmdv@gmail.com>
2020-12-15 18:02:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pycoingecko import CoinGeckoAPI
 from binance.client import Client # Import the Binance Client
 from binance.websockets import BinanceSocketManager # Import the Binance Socket Manager
 import pandas as pd
+import config.py
 
 # turn off warnings
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -9,13 +10,9 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 cg = CoinGeckoAPI()
 
-# Although fine for tutorial purposes, your API Keys should never be placed directly in the script like below. 
-# You should use a config file (cfg or yaml) to store them and reference when needed.
-PUBLIC = 'r7NNoC9Y67xf2mmwSTYM1DwRA03Q3i6YHvKElp9aU6a3LFh0Fhmv0MRPSqBsAt6z'
-SECRET = 'CTfkY0bkUpiLfzEgZjL78X93UhN79Tb26Cqp7W27TSVvhod8vZUR3ACr1Q0B86ju'
 
 # Instantiate a Client 
-client = Client(api_key=PUBLIC, api_secret=SECRET)
+client = Client(api_key=config.api_key, api_secret=config.api_secret)
 
 # Gets data from account balance as dictionary
 coin_balance = client.get_account()

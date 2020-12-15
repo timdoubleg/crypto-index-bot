2bf6e88b4ef68d4113390dce54e42e592be5ebfe
timggraf <tim.g.graf@gmail.com>
2020-12-15 10:13:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,19 +43,32 @@ market_cap['symbol'] = market_cap['symbol'].str.upper() # make the dataframe Upp
 sum_caps = market_cap['market_cap_percentage'].sum() 
 market_cap['market_cap_percentage'] = (market_cap['market_cap_percentage']/sum_caps)
 
-
-# Get market data prices from coingecko
+"""
+# COINGECKO: Get market cap data from 
 tickers = client.get_ticker()
 prices = pd.DataFrame.from_dict(tickers)
-prices_table_columns = ['symbol', 'lastPrice']
+prices_table_columns = ['symbol']
 prices = prices.drop(columns=[col for col in prices if col not in prices_table_columns]) #drop all values not needed
+prices.loc[prices['symbol']=='BTCUSDT'] #check for USDT
 
-
-# As USDTUSDT does not exist we need to append it
+# COINGECKO: As USDTUSDT does not exist we need to append it
 prices.loc[prices['symbol']=='USDTUSDT'] #check for USDT
 prices = prices.append({'symbol': 'USDTUSDT', "lastPrice": 1}, ignore_index=True)
 prices.loc[prices['symbol']=='USDTUSDT'] #check for USDT again, now we find it
 print(prices)
+"""
+
+# BINANCE: Get prices from Binance
+prices_binance = client.get_all_tickers() #gets all prices
+prices_binance = pd.DataFrame.from_dict(prices_binance) #converts dictionary to dataframe
+prices_binance.loc[prices_binance['symbol']=='BTCUSDT'] #check for BTCUSDT, we find it
+
+# As USDTUSDT does not exist we need to append it
+prices_binance.loc[prices_binance['symbol']=='USDTUSDT'] #check for USDT
+prices_binance = prices_binance.append({'symbol': 'USDTUSDT', "price": 1}, ignore_index=True)
+prices_binance.loc[prices_binance['symbol']=='USDTUSDT'] #check for USDT again, now we find it
+print(prices_binance)
+
 
 # create columns for later and some more data handling
 coin_balance['portfolio weights'] = 'NA'
@@ -67,14 +80,14 @@ coin_balance['symbol'] = coin_balance['symbol'].str.upper() # make the dataframe
 
 # merge both dataframes
 coin_balance = coin_balance.rename(columns={'asset': 'symbol'})
-df = pd.merge(prices, coin_balance, how ='inner', on='symbol')
-df['lastPrice'] = pd.to_numeric(df['lastPrice']) #transform to integers
+df = pd.merge(prices_binance, coin_balance, how ='inner', on='symbol')
+df['price'] = pd.to_numeric(df['price']) #transform to integers
 df.dtypes #check it is transformered to integers
 
 
 # calculate portfolio values
 for i in range(len(df)):
-    df['USDT'][i] =  df['lastPrice'][i] * df['free'][i]
+    df['USDT'][i] =  df['price'][i] * df['free'][i]
 
 # calculate portfolio weights
 df['USDT'] = pd.to_numeric(df['USDT'])
@@ -86,15 +99,13 @@ df.loc[df['symbol'] == 'USDTUSDT'] #probably not necessary
 
 
 # drop all not needed values from the df price 
-df_table_columns = ['symbol', 'lastPrice', 'free', 'portfolio weights', 'USDT']
+df_table_columns = ['symbol', 'price', 'free', 'portfolio weights', 'USDT']
 df = df.drop(columns=[col for col in df if col not in df_table_columns])
-market_cap
 
 #merge both dataframes
 df_merged = pd.merge(df, market_cap, how ='left', on='symbol')
 df_merged = df_merged.sort_values(by='market_cap_percentage', ascending=False, na_position='last') #sort by largest to smallest
 df_merged['market_cap_percentage'] = df_merged['market_cap_percentage'].fillna(0)
-
 
 #calculate the differences
 df_merged['difference'] = df_merged['market_cap_percentage'] - df_merged['portfolio weights']
@@ -103,6 +114,7 @@ df_merged['difference'] = df_merged['market_cap_percentage'] - df_merged['portfo
 print(market_cap)
 df_merged = df_merged[(df_merged["free"] != 0) | (df_merged["market_cap_percentage"] != 0)]
 print(df_merged)
+df_merged = df_merged.reset_index(drop=True) # reset index
 
 
 # -------------------------------------------------------------
@@ -124,14 +136,27 @@ order = client.create_test_order(
             quantity = round(df_merged['difference'][i]*threshold*-1*pf_value,2)
             )
 print(order)
+# creating a test order returns an empty response by design if the order would be valid (see the official docs). 
+# You would get an error in the response if there were a problem with it, so the fact that your response is empty means success.
+
 
 # get all open orders
 print(client.get_all_orders(symbol=df_merged['symbol'][i]))
-
-
-
+#if this is empty then we have no open orders
 
 # For Loop for Rebalancing (Work in Progress)
+
+# If we have USDT in our portfolio, we cannot sell directly USDT i, but we buy other cryptos with it
+for i in range(len(df_merged)):
+    if df_merged['symbol'][i] == 'USDTUSDT':
+        print(df_merged['symbol'][i] + ': will not execute this order')
+    else:
+        print(df_merged['symbol'][i] + ': will execute this order')
+
+
+
+
+
 
 for i in range(len(df_merged)):
     #sell order
@@ -140,10 +165,22 @@ for i in range(len(df_merged)):
             symbol= df_merged['symbol'][i],
             side=SIDE_SELL,
             type=ORDER_TYPE_MARKET,
-            timeInForce=TIME_IN_FORCE_GTC,
-            quantity = df_merged['difference'][i]*threshold
+            quantity = round(df_merged['difference'][i]*threshold*-1*pf_value,2)
             )
-        )
+        print(df_merged['symbol'][i] + ': sell order')
+        print(order)
+    #buy order
+    if df_merged['difference'][i] > 0:
+        order = client.create_test_order(
+            symbol= df_merged['symbol'][i],
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            quantity = round(df_merged['difference'][i]*threshold*pf_value,2)
+            )
+        print(df_merged['symbol'][i] +': buy order')
+        print(order)
+
+
 
 
 order = client.create_order(
@@ -155,15 +192,9 @@ order = client.create_order(
     price='0.00001')
 
 
-
-
-
 #without USDT
 df_merged['symbol'] = df_merged['symbol'].str[:-4]
-
-
-
-
+"""
 
 
 

96da2d7135ea72c42092f5d1c229d8c86890c599
Henri de Montpellier <henridmdv@gmail.com>
2020-12-15 18:02:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Although fine for tutorial purposes, your API Keys should never be placed directly in the script like below. 
 # You should use a config file (cfg or yaml) to store them and reference when needed.
-PUBLIC = 'r7NNoC9Y67xf2mmwSTYM1DwRA03Q3i6YHvKElp9aU6a3LFh0Fhmv0MRPSqBsAt6z'
-SECRET = 'CTfkY0bkUpiLfzEgZjL78X93UhN79Tb26Cqp7W27TSVvhod8vZUR3ACr1Q0B86ju'
+api_key = 'r7NNoC9Y67xf2mmwSTYM1DwRA03Q3i6YHvKElp9aU6a3LFh0Fhmv0MRPSqBsAt6z'
+API_SECRET = 'CTfkY0bkUpiLfzEgZjL78X93UhN79Tb26Cqp7W27TSVvhod8vZUR3ACr1Q0B86ju'
